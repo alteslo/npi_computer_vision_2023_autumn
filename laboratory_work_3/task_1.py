aa667fbe7245851bcd9db4109d8c93ee59379c80
Aleksandr Teslenko <alteslo31@gmail.com>
2024-01-13 12:18:49 +0300

    lab3

---
+++ laboratory_work_3/task_1.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+from constants.constant import Images
+
+
+def show_image(image):
+    image = cv2.imread(image)
+    assert image is not None, "file could not be read"
+    # преобразовать изображение в оттенки серого
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # применить фильтр Гаусса для сглаживания изображения
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    # применить алгоритм Canny для обнаружения границ на изображении
+    edges = cv2.Canny(blurred, 50, 150, apertureSize=3)
+
+    # применить преобразование Хафа для поиска линий и окружностей на изображении
+    circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 20,
+                               param1=50, param2=60, minRadius=0, maxRadius=100)
+    lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi /
+                            180, threshold=30, minLineLength=50, maxLineGap=5)
+
+    # отобразить найденные линии и окружности на изображении
+    if circles is not None:
+        circles = np.round(circles[0, :]).astype("int")
+        for (x, y, r) in circles:
+            cv2.circle(image, (x, y), r, (0, 255, 0), 2)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    cv2.imshow('Image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    show_image(Images.IMAGE_1)
