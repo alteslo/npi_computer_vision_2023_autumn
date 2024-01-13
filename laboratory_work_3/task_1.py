f3fbc12821f0992e6f1b43aa9d5753ad40151206
Aleksandr Teslenko <alteslo31@gmail.com>
2024-01-13 12:39:22 +0300

    new method

--- laboratory_work_3/task_1.py
+++ laboratory_work_3/task_1.py
@@ -19,17 +19,32 @@ def show_image(image):
     # применить преобразование Хафа для поиска линий и окружностей на изображении
     circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 20,
                                param1=50, param2=60, minRadius=0, maxRadius=100)
-    lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi /
-                            180, threshold=30, minLineLength=50, maxLineGap=5)
+    # lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi /
+                            # 180, threshold=30, minLineLength=50, maxLineGap=5)
+
+    lines = cv2.HoughLines(edges, rho=1, theta=np.pi / 180, threshold=130)
 
     # отобразить найденные линии и окружности на изображении
     if circles is not None:
         circles = np.round(circles[0, :]).astype("int")
         for (x, y, r) in circles:
             cv2.circle(image, (x, y), r, (0, 255, 0), 2)
+    # if lines is not None:
+    #     for line in lines:
+    #         x1, y1, x2, y2 = line[0]
+    #         cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    
     if lines is not None:
         for line in lines:
-            x1, y1, x2, y2 = line[0]
+            rho, theta = line[0]
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * (a))
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * (a))
             cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
     cv2.imshow('Image', image)
